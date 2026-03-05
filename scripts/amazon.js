268808f7bec5d29714779acd9ab25f1024d0deb4
sindhu <sindhutv150@gmail.com>
2026-03-05 10:33:55 +0530

    make add to cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -3,6 +3,7 @@ let productsHTML = '';
 products.forEach((product) => {
   productsHTML += `
     <div class="product-container">
+
       <div class="product-image-container">
         <img class="product-image"
           src="${product.image}">
@@ -21,10 +22,62 @@ products.forEach((product) => {
       </div>
 
       <div class="product-price">
-        ${(product.priceCents / 100).toFixed(2)}
+        $${(product.priceCents / 100).toFixed(2)}
       </div>
+
+      <!-- ✅ Quantity Dropdown -->
+      <div class="product-quantity-container">
+        <select class="product-quantity">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+
+      <div class="product-spacer"></div>
+
+      <button class="add-to-cart-button button-primary js-add-to-cart" 
+      data-product-id="${product.id}">       
+        Add to Cart
+      </button>
+
     </div>
   `;
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+// To make the button Add to cart work 
+document.querySelectorAll('.js-add-to-cart')
+.forEach((button) => {
+  button.addEventListener ('click', () => {
+   const productId= button.dataset.productId; // this gives what ever the data is attached to the button.
+
+   let matchingItem;
+
+   cart.forEach ((item) => {
+    if(productId === item.productId){
+      matchingItem = item;
+    }
+   })
+
+   if(matchingItem) {
+    matchingItem.quantity += 1;
+   } else {
+    cart.push({
+    productId:productId,
+    quantity :1 
+   });
+   }
+    console.log(cart);
+  })
+
+ 
+})
