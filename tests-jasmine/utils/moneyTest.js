e7ea9bf3d103f476fa3ec58918799e355519e588
sindhu <sindhutv150@gmail.com>
2026-03-18 00:12:40 +0530

    Final updates: removed old tests and added new structure

--- tests-jasmine/utils/moneyTest.js
+++ tests-jasmine/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import {formatCurrency} from "../scripts/utils/money.js";
+import {formatCurrency} from "../../scripts/utils/money.js";
 
 console.log('Converts Cents into dollars');
 if (formatCurrency(2095) === '20.95') {
