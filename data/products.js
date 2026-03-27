0450411cd14d7bc791ceb8c8f6a1aabea7b73cae
sindhu <sindhutv150@gmail.com>
2026-03-27 10:46:29 +0530

    convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import formatCurrency from "../scripts/utils/money.js";
+
 export function getProduct(productId){
  let matchingProduct;
 
@@ -10,6 +12,47 @@ export function getProduct(productId){
   return matchingProduct;
 }
 
+
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails){ 
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarUrl(){
+   return`images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+  getPrice(){
+    return`$${formatCurrency(this.priceCents)}`;
+  }
+
+}
+
+const product1 = new Product({
+   id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+      stars: 4.5,
+      count: 87
+    },
+    priceCents: 1090,
+    keywords: [
+      "socks",
+      "sports",
+      "apparel"
+    ]
+  }
+);
 
 export const products = [
   {
@@ -671,6 +714,11 @@ export const products = [
     ]
     
   }
- 
-];
+].map((productdetails)=> {
+  return new Product(productdetails);
+});
+
+
+
+
 
--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -47,7 +47,7 @@ cartSummaryHtml += `
        ${matchingProduct.name}
       </div>
       <div class="product-price">
-       $${formatCurrency(matchingProduct.priceCents)}
+       ${matchingProduct.getPrice()}
       </div>
       <div class="product-quantity js-product-quantity-${matchingProduct.id}">
         <span>
